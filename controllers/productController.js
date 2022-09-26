14d60c33a15dccc312277c9ea2e8fc72bc0b1535
Shriyarika Nakarmi <shriyarikanakarmi07@gmail.com>
2022-09-26 08:26:00 +0545

    this is the commit to change in the controller file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,25 +1,26 @@
 const ProductModel = require("../models/ProductModel");
-const returnAllProducts = async (req, res) => {
-};
-
-const returnSingleProduct = async (req, res) => {
-};
-
-const createProduct = async (req, res) => {
+const returnAllProducts = async(req, res) => {
 
 };
+console.log("Shriyarika");
 
-const updateProduct = async (req, res) => {
+const returnSingleProduct = async(req, res) => {};
+
+const createProduct = async(req, res) => {
 
 };
-const deleteProduct = async (req, res) => {
+
+const updateProduct = async(req, res) => {
+
+};
+const deleteProduct = async(req, res) => {
 
 };
 
 module.exports = {
-  returnAllProducts,
-  returnSingleProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct,
+    returnAllProducts,
+    returnSingleProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
 };
